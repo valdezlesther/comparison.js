f7a8c17e451d57303b7fc12898525a4f45a0af32
valdezlesther <marklesthervaldez52@gmail.com>
2024-09-13 20:17:12 +0800

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,35 @@
+let firstFavNum = 25
+let secondFavNum = 10
+
+console.log("Greater than:");
+console.log(25 > 10);
+
+console.log("Less than:");
+console.log(25 < 10);
+
+console.log("Greater than or equal to:");
+console.log(25 >= 10);
+
+console.log("Less than or equal to:");
+console.log(25 <= 10);
+
+console.log("Strict equality (===):");
+console.log(25 === 10);
+
+console.log("Loose equality (==):");
+console.log(25 == 10);
+
+console.log("Strict inequality (!==):");
+console.log(25 !== 10);
+
+console.log("Loose inequality (!=):");
+console.log(25 != 10);
+
+console.log("AND");
+console.log((26 > 10) && (10 < 25));
+
+console.log("OR");
+console.log((25 == 10) // (10 == 25));
+
+console.log("NOT");
+console.log(!(25 == 19));
